d58aaee95cb04fb41aee6d0ba939e2533bb280aa
Transmogrifox <transmogrifox@cackleberrypines.net>
2020-03-15 21:28:17 -0700

    Clean up directory structure

--- smps_derivations/octave/reconstruction_filter.m
+++ smps_derivations/octave/reconstruction_filter.m
@@ -1,10 +1,10 @@
 k = 1000;
 u = 1e-6;
 
-fsw = 100*k
+fsw = 130*k
 Ts = 1/fsw;
 
-ff = 10:10:5*fsw;
+ff = 10:10:fsw;
 ws = 2*pi*ff;
 s = j*ws;
 z1 = e.^(-s*Ts);
@@ -22,26 +22,7 @@ iv = 7.5
 mc = Vin/Lm
 md = Vo/Lm
 
-tc = D*Ts;
-%hs = md*(Ts*Dp./s + 1./(s.*s)).*(e.^(-s*Ts) - e.^(-s*Ts*D)) + (mc*D + iv).*(e.^(-s*Ts.*D)).*(1 - e.^(-s*Ts*Dp))./s;
-
-h1 = iv*(1 - e.^(-s*tc))./s;
-h2 = mc*(1 - (1 + s*tc).*e.^(-s*tc))./(s.^2);
-
-hs = h2;
-g0 = 20*log10(abs(h1(1)))
-g0lim =  20*log10(abs(iv*tc))
-
-g1 = 20*log10(abs(h2(1)))
-g1lim =  20*log10(abs((1/2)*mc*tc^2))
-
-c0 = abs(h1(1))
-c0lim = iv*tc
-c1 = abs(h2(1))
-c1lim = (mc*tc^2)/2
-
-glim = 20*log10(fsw*(c0lim + c1lim))
-g = 20*log10(fsw*(abs(h1(1) + h2(1))))
+hs = md*(Ts*Dp./s + 1./(s.*s)).*(e.^(-s*Ts) - e.^(-s*Ts*D)) + (mc*D + iv).*(e.^(-s*Ts.*D)).*(1 - e.^(-s*Ts*Dp))./s;
 
 subplot(2,1,1)
 semilogx(ff, 20*log10(abs(hs)),"r")
--- smps_derivations/octave/rhpz.m
+++ smps_derivations/octave/rhpz.m
@@ -1,15 +1,13 @@
 k = 1000;
 u = 1e-6;
 
-fsw = 130*k*10
+fsw = 130*k
 Ts = 1/fsw;
 
-ff = 10:10:fsw*5;
+ff = 10:10:fsw;
 ws = 2*pi*ff;
 s = j*ws;
 z1 = e.^(-s*Ts);
-
-hizoh = Ts*s./((1-e.^(-s*Ts)));
 
 Lm = 230*u
 Vin = 18.75
@@ -33,14 +31,14 @@ f = (b-md*a*b)/Ts
 g = md*a*b/Ts
 h = (0.5*md*b*b)/Ts
 
-td = a*iv - a*iv + b;
-td = (1-D)*Ts;
-td2 = td*td;
-td2 = a*a*iv*iv + a*a*iv*iv - 2*a*a*iv*iv - 2*a*b*iv + 2*a*b*iv + b*b;
+td = a*iv - a*iv + b
+td = (1-D)*Ts
+td2 = td*td
+td2 = a*a*iv*iv + a*a*iv*iv - 2*a*a*iv*iv - 2*a*b*iv + 2*a*b*iv + b*b
 
-tdio = (a*iv*iv - a*iv*iv + b)*iv;
-hmdtd2 = 0.5*md*td2 ;
-Ioo = (tdio + hmdtd2)/Ts;
+tdio = (a*iv*iv - a*iv*iv + b)*iv
+hmdtd2 = 0.5*md*td2 
+Ioo = (tdio + hmdtd2)/Ts
 
 Ioo = (c + d + ee)*iv*iv + (f + g)*iv + h
 Ioo = (td*iv + 0.5*md*td*td)/Ts
@@ -58,10 +56,6 @@ A1 = (2*d*iv + ee*iv + g)
 hz = (1./(A0 + A1)) .* (A0 + A1*z1);
 hs = (1/wrhpz)*(s - wrhpz);
 %hz = A0 + A1*z1;
-wz = A1/A0 
-wc = fsw*acos( (wz/2)*(2*((1/wz + 1)^2) -1 - 1/(wz^2)) );
-fc = wc/(2000*pi)
 
-
-semilogx(ff, 20*log10(abs(hz)), ff, 20*log10(abs(hz.*hizoh)), ff, 20*log10(abs(hs)) )
+semilogx(ff, 20*log10(abs(hz)), ff, 20*log10(abs(hs)) )
 
