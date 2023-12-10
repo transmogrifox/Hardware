26274718aa63d2f02ea0b5aba55b2878d495ae8b
Transmogrifox <transmogrifox@cackleberrypines.net>
2023-12-09 22:02:41 -0800

    improve transition calculation

--- misc/exponential_decay/expo_lin_decay.c
+++ misc/exponential_decay/expo_lin_decay.c
@@ -2,22 +2,48 @@
 #include <cstdio>
 
 //
-// For theory of implementation, see comments in "expo_decay.c" 
+// For theory of decaying exponential implementation, see comments in "expo_decay.c" 
 //
 
-// In this example, alpha = 1 - 2^-8 will be illustrated
+//
+// To solve for "N" at transition where slope of decaying exponential equals slope of 
+// straight line,
+// N = (-tau/T)*ln(-m*tau/y)                                                       (1)
+//   
+//   T = sampling period
+//   tau = -T/ln((A-1)/A)
+//   m = slope of straight line after transition
+//   y = initial condition for exponential component at time t = 0 (n=0)
+//
+
+// In this example, 
+//    alpha = 1 - 2^-8  (A = 256)
+//    T = 2
+//    m = -100/A = 0.391
+//    tau = 2*255.5  (factor of 2 because it's operated at 2T)
+//    y = 10000 - 1000 = 9000 (initial value minus settled value)
+//
+//    N = -(511/2)*ln(-(-100/256)*511/9000) = 973
+// Since the exponential is operated every other "tick", the total ticks to
+// reach "transition" will be 2*N
+//    transition = 2*973 = 1946
+//
+// Note a more direct computation of "transition" from (1) is to assume T=1, but tau is doubled
+// since the filter is ticked every other sample,
+//   transition = (-2*tau)*ln(-m*2*tau/y) = 2*255.5*ln(-100*2*255.5/9000) = 1946
 
 int main(void)
 {
     unsigned int shift = 8;
-    unsigned int y0 = 10000 << shift;
+    unsigned int ya = 10000;
+    unsigned int y0 = ya << shift;
 
     unsigned int tmp = 0;
 
     // Constant offset value
     unsigned int offst = 1000;
     unsigned int linrate = 100;
-    unsigned int transition = 1800;
+    unsigned int transition = 1946;
 
     // Integral of y[n], proportional to accumulated timing offset
     unsigned int runsum = 0;
@@ -35,8 +61,7 @@ int main(void)
         //Switch between exponential decay vs linear rate based on count of
         //cycles since it began holdover
         //The value of "transition" is computed from the number of cycles at which 
-        //y[n-1] - y[n] == linrate
-        //In this example the value was simply guessed and adjusted graphically
+        //y[n-1] - y[n] == linrate (derivative of exponential matches slope of line)
         if(i<transition)
         {
             //Multiplex either shifted value or a constant every other clock cycle
