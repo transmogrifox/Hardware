6b43ebbb8b24d2198fe908224f9fb1195504c049
Transmogrifox <transmogrifox@cackleberrypines.net>
2025-09-25 22:19:47 -0700

    integral

--- misc/animate/nonlinear_slope_comp.py
+++ misc/animate/nonlinear_slope_comp.py
@@ -22,6 +22,7 @@ def get_mcmpD(Fsw=100.0*kHz, Vout=12.5, Lm=18.0*uH, Nps=10.0/7.3, duty=0.5, gpea
     g = gpeak
     md = (Nps**2.0)*Vout/Lm
     dp = 1.0 - duty
+    mcmp_const = 850000.0
     # Algebraic steps to combine to a single expression
     #mcmp = md*(1+g)/(2*g*duty) - md*dp/duty
     #mcmp = md*(1+g)/(2*g*duty) - md*(1-duty)/duty
@@ -31,7 +32,13 @@ def get_mcmpD(Fsw=100.0*kHz, Vout=12.5, Lm=18.0*uH, Nps=10.0/7.3, duty=0.5, gpea
     mcmp = md*(1+ g*(2*duty - 1))/(2*g*duty)
     mcmp = md*(1/(2*duty))*(1-g)/g + md
     mcmp = (1/duty)*md*(1-g)/(2*g) + md
-    return duty, mcmp
+    mcmp_ = 0.0
+    Hmag = 0.0
+    if duty >= (g-1)/(2*g):
+        mcmp_ = (md/Fsw)*(np.log(duty)*(1-g)/(2*g) + duty + (1-g)/(2*g) + np.log((g-1)/(2*g))*(g-1)/(2*g) )
+        Hmag = 20.0*np.log10( 1.0/(2.0*(mcmp_const/md - 1.0)*duty + 1.0) )
+    
+    return duty, mcmp, mcmp_, Hmag
 
 def get_mcmp(Fsw=100.0*kHz, Vin= 48.0, Vout=12.5, Lm=18.0*uH, Nps=10.0/7.3, gpeak=2.0):
     mc = Vin/Lm
@@ -48,6 +55,8 @@ Dx = []
 mcmp = []
 mcmpx = []
 acmp = []
+acmp_ = []
+Hm = []
 gpk = 2.0
 nps = 10.0/7.3
 lm = 18.0*uH
@@ -59,7 +68,7 @@ dstart = 0.0
 for Vi in np.arange(500.0,9.0,-1.0):
     d0, m0 = get_mcmp(Fsw=fsw, Vin=Vi+1.0, Vout=vo, Lm=lm, Nps=nps, gpeak=gpk)
     da, ma = get_mcmp(Fsw=fsw, Vin=Vi, Vout=vo, Lm=lm, Nps=nps, gpeak=gpk)
-    db, mb = get_mcmpD(Fsw=fsw, Vout=vo, Lm=lm, Nps=nps, duty=da, gpeak=gpk)
+    db, mb, mb_, hmag = get_mcmpD(Fsw=fsw, Vout=vo, Lm=lm, Nps=nps, duty=da, gpeak=gpk)
     
     if(ma > 0.0):
         ac += (da-d0)*ma/fsw
@@ -74,6 +83,8 @@ for Vi in np.arange(500.0,9.0,-1.0):
     mcmp.append(ma)
     mcmpx.append(mb)
     acmp.append(ac)
+    acmp_.append(mb_)
+    Hm.append(hmag)
     
     
 fig = plt.figure()
@@ -96,6 +107,8 @@ iend = imax + istart
 
 plt.subplot(212)
 plt.plot(D, acmp, label="Nonlinear slope comp")
+plt.plot(D, acmp_, label="Nonlinear slope comp")
+plt.plot(D, Hm, label="Nonlinear slope comp")
 plt.plot([0,dstart, maxD], [0,0, iend], label="Linear slope comp")
 
 plt.title("Required slope compensation function of duty")
